323b2fa9a796b0f6e634bc39aa136eecf0b644ed
Frederico Bormann <github@frederico.info>
2020-07-19 20:07:26 +0200

    Add html templates

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"dryck/models"
+	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"log"
+	"net/http"
 )
 
 func main() {
@@ -18,4 +20,25 @@ func main() {
 	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Drink{})
 	db.AutoMigrate(&models.Purchase{})
+
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/", func(c *gin.Context) {
+
+		// Call the HTML method of the Context to render a template
+		c.HTML(
+			// Set the HTTP status to 200 (OK)
+			http.StatusOK,
+			// Use the index.html template
+			"index.html",
+			// Pass the data that the page uses (in this case, 'title')
+			gin.H{
+				"title": "Home Page",
+			},
+		)
+
+	})
+
+	router.Run()
 }
