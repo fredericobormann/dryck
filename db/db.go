491ac8c210bbc42ad68d69d239258516e449994a
Frederico Bormann <freddy.bormann@hotmail.de>
2020-07-24 17:08:46 +0200

    Refactor db.go to fix linter warnings

--- db/db.go
+++ db/db.go
@@ -7,10 +7,12 @@ import (
 	"time"
 )
 
+// DB stores database connection
 type DB struct {
 	Datastore *gorm.DB
 }
 
+// New creates a new database datastore
 func New(dbType string, dbConnInfo string) *DB {
 	db, err := gorm.Open(dbType, dbConnInfo)
 	if err != nil {
@@ -29,6 +31,7 @@ func New(dbType string, dbConnInfo string) *DB {
 	return &dryckdb
 }
 
+// Close closes current database connection
 func (db *DB) Close() {
 	err := db.Datastore.Close()
 	if err != nil {
@@ -36,73 +39,74 @@ func (db *DB) Close() {
 	}
 }
 
-// Returns all users currently in the database
+// GetAllUsers returns all users currently in the database
 func (db *DB) GetAllUsers() []models.User {
 	var allUsers []models.User
 	db.Datastore.Find(&allUsers)
 	return allUsers
 }
 
-// Returns the username for a given user id
-func (db *DB) GetUsername(userId uint) string {
+// GetUsername returns the username for a given user id
+func (db *DB) GetUsername(userID uint) string {
 	var user models.User
-	db.Datastore.Where("id = ?", userId).First(&user)
+	db.Datastore.Where("id = ?", userID).First(&user)
 	return user.Name
 }
 
-// Creates a new user with given username
+// CreateNewUser creates a new user with given username
 func (db *DB) CreateNewUser(username string) {
 	newUser := models.User{Name: username}
 	db.Datastore.Create(&newUser)
 }
 
-// Returns all drinks in the database
+// GetAllDrinks returns all drinks in the database
 func (db *DB) GetAllDrinks() []models.Drink {
 	var allDrinks []models.Drink
 	db.Datastore.Find(&allDrinks)
 	return allDrinks
 }
 
-// Return all purchases of one user specified by user id with drink information preloaded
-func (db *DB) GetPurchasesOfUser(userId uint) []models.Purchase {
+// GetPurchasesOfUser returns all purchases of one user specified by user id with drink information preloaded
+func (db *DB) GetPurchasesOfUser(userID uint) []models.Purchase {
 	var purchases []models.Purchase
-	db.Datastore.Preload("Product").Where("customer_id = ?", userId).Order("purchase_time desc").Find(&purchases)
+	db.Datastore.Preload("Product").Where("customer_id = ?", userID).Order("purchase_time desc").Find(&purchases)
 	return purchases
 }
 
-// Returns the total debt of one user specified by their user id
-func (db *DB) GetTotalDebtOfUser(userId uint) int {
+// GetTotalDebtOfUser returns the total debt of one user specified by their user id
+func (db *DB) GetTotalDebtOfUser(userID uint) int {
 	var totalDebt int
-	db.Datastore.Table("purchases").Where("customer_id = ?", userId).Joins("inner join drinks on purchases.product_id = drinks.id").Select("sum(drinks.price)").Row().Scan(&totalDebt)
+	db.Datastore.Table("purchases").Where("customer_id = ?", userID).Joins("inner join drinks on purchases.product_id = drinks.id").Select("sum(drinks.price)").Row().Scan(&totalDebt)
 	var totalPayed int
-	db.Datastore.Table("payments").Where("user_id = ?", userId).Select("sum(amount)").Row().Scan(&totalPayed)
+	db.Datastore.Table("payments").Where("user_id = ?", userID).Select("sum(amount)").Row().Scan(&totalPayed)
 	return totalDebt - totalPayed
 }
 
-// Adds a purchase for one user specified by their id and a drink also specified by id
-func (db *DB) PurchaseDrink(userId uint, drinkId uint) {
-	purchase := models.Purchase{CustomerID: userId, ProductID: drinkId, PurchaseTime: time.Now()}
+// PurchaseDrink adds a purchase for one user specified by their id and a drink also specified by id
+func (db *DB) PurchaseDrink(userID uint, drinkID uint) {
+	purchase := models.Purchase{CustomerID: userID, ProductID: drinkID, PurchaseTime: time.Now()}
 	db.Datastore.Create(&purchase)
 }
 
-// Delete a purchase from database
-func (db *DB) DeletePurchase(purchaseId uint) {
-	db.Datastore.Where("id = ?", purchaseId).Unscoped().Delete(&models.Purchase{})
+// DeletePurchase deletes a purchase from database
+func (db *DB) DeletePurchase(purchaseID uint) {
+	db.Datastore.Where("id = ?", purchaseID).Unscoped().Delete(&models.Purchase{})
 }
 
-// Adds a payment for a user specified by id with a certain amount
-func (db *DB) AddPayment(userId uint, amount int) {
-	payment := models.Payment{UserID: userId, Amount: amount, PaymentTime: time.Now()}
+// AddPayment adds a payment for a user specified by id with a certain amount
+func (db *DB) AddPayment(userID uint, amount int) {
+	payment := models.Payment{UserID: userID, Amount: amount, PaymentTime: time.Now()}
 	db.Datastore.Create(&payment)
 }
 
-func (db *DB) DeletePayment(paymentId uint) {
-	db.Datastore.Where("id = ?", paymentId).Unscoped().Delete(&models.Payment{})
+// DeletePayment deletes a payment specified by its id
+func (db *DB) DeletePayment(paymentID uint) {
+	db.Datastore.Where("id = ?", paymentID).Unscoped().Delete(&models.Payment{})
 }
 
-// Returns all payments of one user specified by id
-func (db *DB) GetAllPaymentsOfUser(userId uint) []models.Payment {
+// GetAllPaymentsOfUser returns all payments of one user specified by id
+func (db *DB) GetAllPaymentsOfUser(userID uint) []models.Payment {
 	var payments []models.Payment
-	db.Datastore.Where("user_id = ?", userId).Find(&payments)
+	db.Datastore.Where("user_id = ?", userID).Find(&payments)
 	return payments
 }
